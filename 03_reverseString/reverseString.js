08e5a30292906f5a7b576ec6623bd49fe305994d
sumustapha <sumustapha@gmail.com>
2022-07-14 07:35:16 +0100

    reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(forward) {
+let string = forward;
+let reverse = "";
+let stringLength = string.length;
+for(let i = (stringLength -1); i>=0; i--)
+{
+    reverse+=string[i];
+    console.log(reverse);
+}
+return reverse;
 };
 
 // Do not edit below this line

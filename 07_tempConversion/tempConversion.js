077f31d22a7f51f0cbeef8195748747b85c080e7
sumustapha <sumustapha@gmail.com>
2022-07-14 22:20:54 +0100

    tempConversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+const tempf = parseFloat(f);
+return (Math.round(((tempf - 32) * (5/9))*10)/10);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  const tempc = parseFloat(c);
+  return (Math.round(((tempc * (9/5)) + 32)*10)/10);
 };
 
 // Do not edit below this line

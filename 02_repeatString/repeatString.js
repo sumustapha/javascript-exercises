9d432e95bf7817198edf12bc853bbadf58d546a9
sumustapha <sumustapha@gmail.com>
2022-07-14 06:54:41 +0100

    updated after repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word,n) {
+    let str = word;
+  if((n===0)||(str==="")){
+    str = "";
+  }
+  else if(n<0)
+  {
+    str = "ERROR";
+  }
+  else
+  {
+    str+=repeatString(str, (n-1));
+  }
+return str;
 
 };
 
